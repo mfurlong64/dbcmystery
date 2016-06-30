7bbe08b7bd8ed94b6408bde8776da4e13d3aded5
Michael Furlong <email": null>
2016-06-30 14:30:16 -0700

    being to build user views and controller methods

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,14 @@
+get '/users' do
+  @users = User.all #define instance variable for view
+  @current_user = nil
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])    # Finds the current user if active
+  end
+  erb :'users/index' #show all users view (index)
+end
+
 get '/users/login' do
-  "Hello World"
+  erb :'users/login'
 end
 
 get '/users/new' do
