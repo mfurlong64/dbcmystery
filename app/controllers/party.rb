32357d0ea4fbd1b334e68b3ed0a26146f36080cb
Michael Furlong <email": null>
2016-06-30 17:27:25 -0700

    continue to build routes for parties

--- app/controllers/party.rb
+++ app/controllers/party.rb
@@ -10,6 +10,17 @@ get '/parties/:id' do
     # If the user is not in the session the move on
 end
 
+# grabs the party id from the params if the user has that part
+# auto logs that player if he has that party
+
 get '/parties/:id/login' do
-  current_user ==
+  current_user.parties.each do |party|
+    if party.id == params[:id]
+      session[:party_id] = party.id
+      erb :"parties/#{session[:party_id]}/show"
+    end
+  end
+
+  @party = Party.find(params[:id])
+  erb :'/parties/login'
 end
